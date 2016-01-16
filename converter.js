837f76f04676ab2db8bd2910f4da59ee69bf6989
Sylvia Ma <xiangwma@umich.edu>
2016-01-16 13:49:41 -0600

    finished for Converter

--- converter.js
+++ converter.js
@@ -1,22 +1,18 @@
 
-function FtoC(f) {
-	var out= (parseFloat(f)-32) *5/9;
+function FtoC() {
+	var inputValue=document.getElementById("input").value;
+	var out= (parseFloat(inputValue)-32) *5/9;
 	document.getElementById("output").innerHTML = out;
 	console.log(out);
 }
 
-/*
-function trig() {
-var a= document.getElementById("input").value;
-FtoC(a);
-}
-*/
 
 
-
-function CtoF(c) {
-	var out= c*9/5+32;
-	document.getElementById("output").innerHTML = out;
+function CtoF() {
+	var inputValue=document.getElementById("input").value;
+	var out= inputValue *9/5+32;
+  document.getElementById("output").innerHTML = out;
+	console.log (out);
 }
 
 
@@ -29,6 +25,8 @@ function determineConverter(){
 	}
 }
 
-/*
-button.getElementById("myBtn").addEventListener("click", determineConverter);
+
+
+/* I don't know how to use this
+button.addEventListener("click", determineConverter);
 */
